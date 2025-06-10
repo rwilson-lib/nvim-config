8c2ee284c7d82ee08add22a8564a6d0771967929
Ramus Jabee Lloyd Wilson <ramus@rjlwjr.com>
2025-06-10 14:34:00 +0000

    Added support to flash line with yank

--- init.lua
+++ init.lua
@@ -1,3 +1,13 @@
-
-require("core")
 require("config.lazy")
+require("core")
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    local event = vim.v.event
+    if event.operator == "y" and event.regtype == "V" then
+      vim.highlight.on_yank { higroup = "IncSearch", timeout = 150 }
+    end
+  end,
+  desc = "Highlight yanked lines",
+
+})
