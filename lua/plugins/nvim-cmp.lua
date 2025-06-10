0701452ca741bd1f92e20e05bc240b00eae499f3
Ramus Jabee Lloyd Wilson <ramus@rjlwjr.com>
2025-06-10 14:32:01 +0000

    Added Dbee completion dependency and config a file local options

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -3,44 +3,79 @@ return {
   event = "InsertEnter",
   dependencies = {
     "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path", -- source for text in buffer
+    "hrsh7th/cmp-path",             -- source for text in buffer
     { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
-    "saadparwaiz1/cmp_luasnip", -- for autocompletion
+    "saadparwaiz1/cmp_luasnip",     -- for autocompletion
     "rafamadriz/friendly-snippets", -- useful snippets
+    {
+      "MattiasMTS/cmp-dbee",
+      dependencies = {
+        { "kndndrj/nvim-dbee" }
+      },
+      ft = "sql", -- optional but good to have
+      opts = {},  -- needed
+    },
   },
   config = function()
     local cmp = require("cmp")
     local luasnip = require("luasnip")
-    -- Load VSCode-style snippets from installed plugins (e.g. friendly-snippets)
+
     require("luasnip.loaders.from_vscode").lazy_load()
 
+    -- Global cmp setup (excluding cmp-dbee)
     cmp.setup({
       completion = {
         completeopt = "menu,menuone,preview,noselect",
       },
-      snippet = { -- Configure how nvim-cmp interacts with snippet engine
+      snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
         end,
       },
-      mapping = cmp.mapping.preset.insert({ -- Corrected mapping preset usage
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- Previous suggestion
-        ["<C-j>"] = cmp.mapping.select_next_item(), -- Next suggestion
+      mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(), -- Show completion suggestions
-        ["<C-e>"] = cmp.mapping.abort(), -- Close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Confirm selection
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip and luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip and luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
       }),
-      -- Sources for autocompletion
       sources = cmp.config.sources({
-        { name = "nvim_lsp" }, -- lsp
-        { name = "luasnip" }, -- Snippets
-        { name = "buffer" },  -- Text in current buffer
-        { name = "path" },    -- File system paths
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+      }),
+    })
+
+    -- SQL-specific setup with cmp-dbee
+    cmp.setup.filetype("sql", {
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+        { name = "cmp-dbee" }, -- only here
       }),
     })
   end,
 }
-
-
