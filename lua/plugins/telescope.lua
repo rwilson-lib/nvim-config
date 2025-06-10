c8a29b4c942e03920bca63711ee7beeccc90bab2
Ramus Jabee Lloyd Wilson <ramus@rjlwjr.com>
2025-06-10 13:46:13 +0000

    Remove overlapping keymaps form Telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,32 +1,37 @@
-
 return {
   'nvim-telescope/telescope.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     'nvim-tree/nvim-web-devicons'
-},  -- Telescope dependency
+  }, -- Telescope dependency
 
   config = function()
     local builtin = require('telescope.builtin')
-    local actions = require ("telescope.actions")
+    local actions = require("telescope.actions")
 
     -- Key mappings for normal mode
+
     vim.keymap.set('n', 'g/f', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', 'g/g', builtin.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', 'g/b', builtin.buffers, { desc = 'Telescope buffers' })
-    vim.keymap.set('n', 'g/B', builtin.buffers, { desc = 'Telescope scope buffers' })
     vim.keymap.set('n', 'g/?', builtin.help_tags, { desc = 'Telescope help tags' })
+    vim.keymap.set('n', 'g/k', builtin.keymaps, { desc = 'Telescope keymaps' })
+    vim.keymap.set('n', '<leader>ec', function()
+      builtin.find_files {
+        cmd = vim.fn.stdpath("config")
+      }
+    end, { desc = 'Telescope edit config' })
 
-    require('telescope').setup{
+    require('telescope').setup {
       defaults = {
-        path_display = {"smart"},
+        path_display = { "smart" },
         mappings = {
           i = {
             -- Map actions.which_key to <C-h> (default: <C/>)
             ["<C-h>"] = "which_key",
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
+            ["<C-j>"] = actions.move_selection_next,     -- move to next result
             ["<C-q›"] = actions.send_selected_to_qflist + actions.open_qflist,
           },
         },
@@ -40,7 +45,7 @@ return {
     }
 
     -- Load any extensions you need
-     require('telescope').load_extension('fzf')
-     require("telescope").load_extension("scope")
+    require('telescope').load_extension('fzf')
+    require("telescope").load_extension("scope")
   end
 }
