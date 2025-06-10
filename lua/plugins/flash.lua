4df6b32f0d2f997c3270528db80966a6e3f0b301
Ramus Jabee Lloyd Wilson <ramus@rjlwjr.com>
2025-06-10 14:30:18 +0000

    removed unwanted imports

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,8 +1,6 @@
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
-  ---@type Flash.Config
-  opts = {},
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
