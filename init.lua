01897a11d8976b59c76910febf784c9d32e8872c
Ramus Jabee Lloyd Wilson <ramus@rjlwjr.com>
2025-06-11 06:45:18 +0000

    Improved flash indicator to work with all yank operation

--- init.lua
+++ init.lua
@@ -4,10 +4,11 @@ require("core")
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
     local event = vim.v.event
-    if event.operator == "y" and event.regtype == "V" then
+    -- Check if the operator was 'y' (yank)
+    -- And if the selection type was 'v' (character-wise), 'V' (line-wise), or 'b' (block-wise)
+    if event.operator == "y" and (event.regtype == "v" or event.regtype == "V" or event.regtype == "b") then
       vim.highlight.on_yank { higroup = "IncSearch", timeout = 150 }
     end
   end,
-  desc = "Highlight yanked lines",
-
+  desc = "Highlight yanked text (all types)",
 })
