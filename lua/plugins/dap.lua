8745f3ddb30fe9d9972340b48f19afb30868b456
Ramus Jabee Lloyd Wilson <ramus@rjlwjr.com>
2025-06-11 13:53:36 +0000

    Formatted buffer

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -7,13 +7,13 @@ return {
     "theHamsta/nvim-dap-virtual-text",
     "leoluz/nvim-dap-go",
   },
-	config = function()
-	  local dap = require("dap")
+  config = function()
+    local dap = require("dap")
     local dapui = require("dapui")
 
-	   require("dap-go").setup()
-	   require("dapui").setup()
-     require("nvim-dap-virtual-text").setup()
+    require("dap-go").setup()
+    require("dapui").setup()
+    require("nvim-dap-virtual-text").setup()
 
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
@@ -28,7 +28,33 @@ return {
       dapui.close()
     end
 
-	   vim.keymap.set('n', '<Leader>dt', dap.toggle_breakpoint, {})
-	   vim.keymap.set('n', '<Leader>dc', dap.continue, {})
-	end
+    vim.keymap.set({ 'n', 'i' }, '<F5>', "<cmd>DapContinue<cr>", { desc = 'DAP: Continue' })
+    vim.keymap.set({ 'n', 'i' }, '<F10>', "<cmd>DapStepOver<cr>", { desc = 'DAP: Step Over' })
+    vim.keymap.set({ 'n', 'i' }, '<F11>', "<cmd>DapStepInto<cr>", { desc = 'DAP: Step Into' })
+    vim.keymap.set({ 'n', 'i' }, '<F12>', "<cmd>DapStepOut<cr>", { desc = 'DAP: Step Out' })
+    vim.keymap.set({ 'n', 'i' }, '<F8>', "<cmd>DapToggleBreakpoint<cr>", { desc = 'DAP: Toggle Breakpoint' })
+    vim.keymap.set({ 'n', 'i' }, '<F9>', function()
+      dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
+    end, { desc = 'DAP: Set Log Point' })
+
+    vim.keymap.set('n', '<Leader>dr', function() dap.repl.open() end, { desc = 'DAP: Open REPL' })
+    vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end, { desc = 'DAP: Run Last' })
+
+    vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
+      require('dap.ui.widgets').hover()
+    end, { desc = 'DAP: Hover' })
+
+    vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
+      require('dap.ui.widgets').preview()
+    end, { desc = 'DAP: Preview' })
+
+    vim.keymap.set('n', '<Leader>df', function()
+      local widgets = require('dap.ui.widgets')
+      widgets.centered_float(widgets.frames)
+    end, { desc = 'DAP: Show Frames' })
+
+    vim.keymap.set('n', '<Leader>ds', function()
+      require('dap.ui.widgets').toggle_scopes()
+    end, { desc = 'DAP: Toggle Scopes' })
+  end
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,31 +1,31 @@
 return {
-  'nvim-telescope/telescope.nvim',
+  "nvim-telescope/telescope.nvim",
   dependencies = {
-    'nvim-lua/plenary.nvim',
+    "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    'nvim-tree/nvim-web-devicons'
+    "nvim-tree/nvim-web-devicons",
   }, -- Telescope dependency
 
   config = function()
-    local builtin = require('telescope.builtin')
+    local builtin = require("telescope.builtin")
     local actions = require("telescope.actions")
 
     -- Key mappings for normal mode
 
-    vim.keymap.set('n', 'g/f', builtin.find_files, { desc = 'Telescope find files' })
-    vim.keymap.set('n', 'g/g', builtin.live_grep, { desc = 'Telescope live grep' })
-    vim.keymap.set('n', 'g/b', builtin.buffers, { desc = 'Telescope buffers' })
-    vim.keymap.set('n', 'g/?', builtin.help_tags, { desc = 'Telescope help tags' })
-    vim.keymap.set('n', 'g/k', builtin.keymaps, { desc = 'Telescope keymaps' })
-    vim.keymap.set('n', 'g/q', builtin.quickfix, { desc = 'Telescope Quickfix' })
-    vim.keymap.set('n', 'g/Q', builtin.quickfixhistory, { desc = 'Telescope Quickfix hist' })
-    vim.keymap.set('n', '<leader>ec', function()
-      builtin.find_files {
-        cmd = vim.fn.stdpath("config")
-      }
-    end, { desc = 'Telescope edit config' })
+    vim.keymap.set("n", "g/f", builtin.find_files, { desc = "Telescope find files" })
+    vim.keymap.set("n", "g/g", builtin.live_grep, { desc = "Telescope live grep" })
+    vim.keymap.set("n", "g/b", builtin.buffers, { desc = "Telescope buffers" })
+    vim.keymap.set("n", "g/?", builtin.help_tags, { desc = "Telescope help tags" })
+    vim.keymap.set("n", "g/k", builtin.keymaps, { desc = "Telescope keymaps" })
+    vim.keymap.set("n", "g/q", builtin.quickfix, { desc = "Telescope Quickfix" })
+    vim.keymap.set("n", "g/Q", builtin.quickfixhistory, { desc = "Telescope Quickfix hist" })
+    vim.keymap.set("n", "<leader>ec", function()
+      builtin.find_files({
+        cmd = vim.fn.stdpath("config"),
+      })
+    end, { desc = "Telescope edit config" })
 
-    require('telescope').setup {
+    require("telescope").setup({
       defaults = {
         path_display = { "smart" },
         mappings = {
@@ -33,7 +33,7 @@ return {
             -- Map actions.which_key to <C-h> (default: <C/>)
             ["<C-h>"] = "which_key",
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next,     -- move to next result
+            ["<C-j>"] = actions.move_selection_next, -- move to next result
             ["<C-q›"] = actions.send_selected_to_qflist + actions.open_qflist,
           },
         },
@@ -43,11 +43,11 @@ return {
       },
       extensions = {
         -- Configuration for extensions
-      }
-    }
+      },
+    })
 
     -- Load any extensions you need
-    require('telescope').load_extension('fzf')
+    require("telescope").load_extension("fzf")
     require("telescope").load_extension("scope")
-  end
+  end,
 }
