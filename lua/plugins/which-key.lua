7ce9e4dd4531f9930bd00351029204321b43f2be
Ramus Jabee Lloyd Wilson <ramus@rjlwjr.com>
2025-06-11 15:07:07 +0000

    Created new spec mapping for Gitsigns

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,6 +5,10 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
+    spec = {
+      { ";g", group = "Gitsigns" },
+      { "g/", group = "Telescope" },
+    },
   },
   keys = {
     {
