a92ea1e83a2289455dd94541fb6ad46af7fcdb9d
Ramus Jabee Lloyd Wilson <ramus@rjlwjr.com>
2025-06-15 22:36:11 +0000

    refactor: replaced nvim-cmp with blink-cmp for faster completion

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,252 @@
+return {
+  {
+    "xzbdmw/colorful-menu.nvim",
+    config = function()
+      -- You don't need to set these options.
+      require("colorful-menu").setup({
+        ls = {
+          lua_ls = {
+            -- Maybe you want to dim arguments a bit.
+            arguments_hl = "@comment",
+          },
+          gopls = {
+            -- By default, we render variable/function's type in the right most side,
+            -- to make them not to crowd together with the original label.
+
+            -- when true:
+            -- foo             *Foo
+            -- ast         "go/ast"
+
+            -- when false:
+            -- foo *Foo
+            -- ast "go/ast"
+            align_type_to_right = true,
+            -- When true, label for field and variable will format like "foo: Foo"
+            -- instead of go's original syntax "foo Foo". If align_type_to_right is
+            -- true, this option has no effect.
+            add_colon_before_type = false,
+            -- See https://github.com/xzbdmw/colorful-menu.nvim/pull/36
+            preserve_type_when_truncate = true,
+          },
+          -- for lsp_config or typescript-tools
+          ts_ls = {
+            -- false means do not include any extra info,
+            -- see https://github.com/xzbdmw/colorful-menu.nvim/issues/42
+            extra_info_hl = "@comment",
+          },
+          vtsls = {
+            -- false means do not include any extra info,
+            -- see https://github.com/xzbdmw/colorful-menu.nvim/issues/42
+            extra_info_hl = "@comment",
+          },
+          ["rust-analyzer"] = {
+            -- Such as (as Iterator), (use std::io).
+            extra_info_hl = "@comment",
+            -- Similar to the same setting of gopls.
+            align_type_to_right = true,
+            -- See https://github.com/xzbdmw/colorful-menu.nvim/pull/36
+            preserve_type_when_truncate = true,
+          },
+          clangd = {
+            -- Such as "From <stdio.h>".
+            extra_info_hl = "@comment",
+            -- Similar to the same setting of gopls.
+            align_type_to_right = true,
+            -- the hl group of leading dot of "•std::filesystem::permissions(..)"
+            import_dot_hl = "@comment",
+            -- See https://github.com/xzbdmw/colorful-menu.nvim/pull/36
+            preserve_type_when_truncate = true,
+          },
+          zls = {
+            -- Similar to the same setting of gopls.
+            align_type_to_right = true,
+          },
+          roslyn = {
+            extra_info_hl = "@comment",
+          },
+          dartls = {
+            extra_info_hl = "@comment",
+          },
+          -- The same applies to pyright/pylance
+          basedpyright = {
+            -- It is usually import path such as "os"
+            extra_info_hl = "@comment",
+          },
+          pylsp = {
+            extra_info_hl = "@comment",
+            -- Dim the function argument area, which is the main
+            -- difference with pyright.
+            arguments_hl = "@comment",
+          },
+          -- If true, try to highlight "not supported" languages.
+          fallback = true,
+          -- this will be applied to label description for unsupport languages
+          fallback_extra_info_hl = "@comment",
+        },
+        -- If the built-in logic fails to find a suitable highlight group for a label,
+        -- this highlight is applied to the label.
+        fallback_highlight = "@variable",
+        -- If provided, the plugin truncates the final displayed text to
+        -- this width (measured in display cells). Any highlights that extend
+        -- beyond the truncation point are ignored. When set to a float
+        -- between 0 and 1, it'll be treated as percentage of the width of
+        -- the window: math.floor(max_width * vim.api.nvim_win_get_width(0))
+        -- Default 60.
+        max_width = 60,
+      })
+    end,
+  },
+  {
+    "saghen/blink.compat",
+    version = "*",
+    lazy = true,
+    opts = {
+      impersonate_nvim_cmp = false,
+      -- print some debug information. Might be useful for troubleshooting
+      debug = false,
+    },
+  },
+  {
+    -- https://cmp.saghen.dev/installation.html
+    -- https://www.youtube.com/watch?v=GKIxgCcKAq4&t=629s
+    "saghen/blink.cmp",
+    event = "InsertEnter",
+    -- optional: provides snippets for the snippet source
+    dependencies = {
+      "disrupted/blink-cmp-conventional-commits",
+      "rafamadriz/friendly-snippets",
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+      -- nvim-cmp sources
+      -- requires saghen/blink.compat
+      -- https://github.com/hrsh7th/nvim-cmp/wiki/List-of-sources
+      "MattiasMTS/cmp-dbee",
+      {
+        "Kaiser-Yang/blink-cmp-dictionary",
+        lazy = true,
+        dependencies = { "nvim-lua/plenary.nvim" },
+      },
+    },
+
+    -- use a release tag to download pre-built binaries
+    version = "1.*",
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = {
+        preset = "default",
+        ["<Tab>"] = { "accept", "fallback" },
+      },
+
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = "mono",
+      },
+
+      -- (Default) Only show the documentation popup when manually triggered
+      completion = {
+        documentation = { auto_show = true },
+        ghost_text = {
+          enabled = true,
+          show_with_menu = false,
+        },
+        menu = {
+          auto_show = false,
+          border = "rounded",
+          draw = {
+            -- We don't need label_description now because label and label_description are already
+            -- combined together in label by colorful-menu.nvim.
+            columns = { { "kind_icon" }, { "label", gap = 1 } },
+            components = {
+              label = {
+                text = function(ctx)
+                  return require("colorful-menu").blink_components_text(ctx)
+                end,
+                highlight = function(ctx)
+                  return require("colorful-menu").blink_components_highlight(ctx)
+                end,
+              },
+            },
+          },
+        },
+      },
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      signature = {
+        enabled = true,
+      },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer", "dictionary" },
+        per_filetype = {
+          sql = { "snippets", "dadbod", "buffer" },
+          gitcomit = { "buffer", "conventional_commits" },
+        },
+        providers = {
+          dictionary = {
+            module = "blink-cmp-dictionary",
+            name = "Dict",
+            -- Make sure this is at least 2.
+            -- 3 is recommended
+            min_keyword_length = 3,
+            opts = {
+              -- options for blink-cmp-dictionary
+            },
+          },
+          dadbod = {
+            name = "Dadbod",
+            module = "vim_dadbod_completion.blink",
+          },
+          conventional_commits = {
+            name = "Conventional Commits",
+            module = "blink-cmp-conventional-commits",
+            score_offset = 0,
+            -- enabled = function()
+            --   return vim.bo.filetype == "gitcommit"
+            -- end,
+            ---@module 'blink-cmp-conventional-commits'
+            ---@type blink-cmp-conventional-commits.Options
+            opts = {}, -- none so far
+          },
+          -- cmp_dbee = {
+          --   name = "cmp-dbee",
+          --   module = "blink.compat.source",
+          --   score_offset = 0,
+          --   -- enabled = function()
+          --   --   return vim.bo.filetype == "sql"
+          --   -- end,
+          --   opts = {
+          --     debug = true,
+          --   },
+          -- },
+        },
+      },
+
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = "prefer_rust_with_warning" },
+    },
+    opts_extend = { "sources.default" },
+  },
+}
--- lua/plugins/nvim-lsp.lua
+++ lua/plugins/nvim-lsp.lua
@@ -3,18 +3,91 @@ return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
-    "neovim/nvim-lspconfig",
     "hrsh7th/cmp-nvim-lsp",
-    { "mason-org/mason.nvim",                opts = {} },
+    { "mason-org/mason.nvim", opts = {} },
     { "antosha417/nvim-lsp-file-operations", config = true },
-    { "folke/neodev.nvim",                   opts = {} },
+    { "folke/neodev.nvim", opts = {} },
+    {
+      "SmiteshP/nvim-navbuddy",
+      dependencies = {
+        "SmiteshP/nvim-navic",
+        "MunifTanjim/nui.nvim",
+      },
+      opts = { lsp = { auto_attach = true } },
+    },
   },
-  config = function()
-    local lspconfig = require("lspconfig")
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-    local util = require("lspconfig.util")
+  opts = {
+    servers = {
+      graphql = {
+        filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+      },
+      emmet_ls = {
+        filetypes = { "html", "css", "sass", "scss", "less", "typescriptreact", "javascriptreact", "svelte" },
+      },
+      clangd = {
+        filetypes = { "c", "cpp" },
+        cmd = { "clangd" },
+      },
+      gopls = {
+        settings = {
+          gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            semanticTokens = true,
+            staticcheck = true,
+            analyses = { unusedparams = true },
+          },
+        },
+      },
+      lua_ls = {
+        -- Server-specific settings. See `:help lsp-quickstart`
+        settings = {
+          lua_ls = {
+            runtime = {
+              version = "LuaJIT",
+            },
+            hint = {
+              enable = true, -- ✅ enable inlay hints
+              -- arrayIndex = "Auto",
+              -- await = true,
+              -- paramName = "All", -- Show parameter names
+              -- paramType = true, -- Show parameter types
+              -- semicolon = "Disable",
+              -- setType = true, -- Show variable types
+            },
+            diagnostics = {
+              globals = { "vim" },
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = true,
+            },
+            completion = { callSnippet = "Replace" },
+          },
+        },
+      },
+    },
+  },
 
-    local capabilities = cmp_nvim_lsp.default_capabilities()
+  config = function(_, opts)
+    local lspconfig = require("lspconfig")
+    -- local util = require("lspconfig.util")
+    -- local navic = require("nvim-navic")
+    --
+
+    -- config = function()
+    --   local on_attach = function(client, bufnr)
+    --     if client.server_capabilities.documentSymbolProvider then
+    --       navic.attach(client, bufnr)
+    --     end
+    --   end
+
+    -- end,
+
+    for server, config in pairs(opts.servers) do
+      config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+      lspconfig[server].setup(config)
+    end
 
     -- Set diagnostic icons
     vim.diagnostic.config({
@@ -22,9 +95,9 @@ return {
       signs = {
         text = {
           [vim.diagnostic.severity.ERROR] = " ",
-          [vim.diagnostic.severity.WARN]  = " ",
-          [vim.diagnostic.severity.HINT]  = "󰠠 ",
-          [vim.diagnostic.severity.INFO]  = " ",
+          [vim.diagnostic.severity.WARN] = " ",
+          [vim.diagnostic.severity.HINT] = "󰠠 ",
+          [vim.diagnostic.severity.INFO] = " ",
         },
       },
     })
@@ -34,127 +107,26 @@ return {
       group = vim.api.nvim_create_augroup("UserLspConfig", {}),
       callback = function(ev)
         local keymap = vim.keymap.set
-        local opts = function(desc)
+        local options = function(desc)
           desc = desc or "" --
           return { buffer = ev.buf, silent = true, desc = desc }
         end
 
-        keymap("n", "gD", vim.lsp.buf.declaration, opts("lsp declaration"))
-        keymap("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts("Telescope lsp_definitions"))
-        keymap("n", "gR", "<cmd>Telescope lsp_references<CR>", opts("Telescope lsp_references"))
-        keymap("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts("Telescope lsp_implementations"))
-        keymap("n", "K", vim.lsp.buf.hover, opts("Popup Docs"))
-        keymap("n", "g=", vim.lsp.buf.format, opts("lsp format"))
+        keymap("n", "gD", vim.lsp.buf.declaration, options("lsp declaration"))
+        keymap("n", "gd", "<cmd>Telescope lsp_definitions<CR>", options("Telescope lsp_definitions"))
+        keymap("n", "gR", "<cmd>Telescope lsp_references<CR>", options("Telescope lsp_references"))
+        keymap("n", "gi", "<cmd>Telescope lsp_implementations<CR>", options("Telescope lsp_implementations"))
+        keymap("n", "K", vim.lsp.buf.hover, options("Popup Docs"))
+        keymap("n", "g=", vim.lsp.buf.format, options("lsp format"))
 
-        keymap("n", "<leader>lcd", "<cmd>Telescope diagnostics<CR>", opts("Telescope lsp ws diag"))
-        keymap('n', 'g/S', "<cmd>Telescope lsp_workspace_symbols<CR>", opts("Telescope lsp ws sym"))
-        keymap('n', 'g/s', "<cmd>Telescope lsp_document_symbols<CR>", opts("Telescope lsp sym"))
+        keymap("n", "<leader>lcd", "<cmd>Telescope diagnostics<CR>", options("Telescope lsp ws diag"))
+        keymap("n", "g/S", "<cmd>Telescope lsp_workspace_symbols<CR>", options("Telescope lsp ws sym"))
+        keymap("n", "g/s", "<cmd>Telescope lsp_document_symbols<CR>", options("Telescope lsp sym"))
 
-        keymap("n", '<leader>lch',
-          function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-          end, opts("[T]oggle Inlay [H]ints"))
+        keymap("n", "<leader>lch", function()
+          vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+        end, options("[T]oggle Inlay [H]ints"))
       end,
-    })
-
-    -- Handler map
-    local custom_handlers = {
-      svelte = function()
-        lspconfig.svelte.setup({
-          capabilities = capabilities,
-          on_attach = function(client)
-            vim.api.nvim_create_autocmd("BufWritePost", {
-              pattern = { "*.js", "*.ts" },
-              callback = function(ctx)
-                local uri = vim.uri_from_fname(ctx.file)
-                client.notify("$/onDidChangeTsOrJsFile", { uri = uri })
-              end,
-            })
-          end,
-        })
-      end,
-
-      gopls = function()
-        lspconfig.gopls.setup({
-          -- on_attach = on_attach
-          cmd = { "gopls" },
-          capabilities = capabilities,
-          filetypes = { "go", "gomod", "gowork", "gotmpl" },
-          root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-          settings = {
-            gopls = {
-              completeUnimported = true,
-              usePlaceholders = true,
-              semanticTokens = true,
-              staticcheck = true,
-              analyses = { unusedparams = true },
-            },
-          },
-        })
-      end,
-
-      graphql = function()
-        lspconfig.graphql.setup({
-          capabilities = capabilities,
-          filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-        })
-      end,
-
-      emmet_ls = function()
-        lspconfig.emmet_ls.setup({
-          capabilities = capabilities,
-          filetypes = {
-            "html", "css", "sass", "scss", "less",
-            "typescriptreact", "javascriptreact", "svelte",
-          },
-        })
-      end,
-
-      lua_ls = function()
-        lspconfig.lua_ls.setup({
-          capabilities = capabilities,
-          settings = {
-            Lua = {
-              runtime = {
-                version = 'LuaJIT'
-              },
-              hint = {
-                enable = true, -- ✅ enable inlay hints
-                -- arrayIndex = "Auto",
-                -- await = true,
-                -- paramName = "All", -- Show parameter names
-                -- paramType = true, -- Show parameter types
-                -- semicolon = "Disable",
-                -- setType = true, -- Show variable types
-              },
-              diagnostics = {
-                globals = { "vim" }
-              },
-              workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
-                checkThirdParty = true,
-              },
-              completion = { callSnippet = "Replace" },
-            },
-          },
-        })
-      end,
-    }
-
-    require('mason-lspconfig').setup({
-      ensure_installed = {
-        'lua_ls', 'gopls',
-        -- add other servers here
-      },
-      automatic_enable = true,
-      handlers = {
-        -- this first function is the "default handler"
-        -- it applies to every language server without a "custom handler"
-        function(server_name)
-          require('lspconfig')[server_name].setup({})
-        end,
-        custom_handlers,
-      }
     })
   end,
 }
