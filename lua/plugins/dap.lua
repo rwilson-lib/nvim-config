d941649fc2be2c2908218120d4e2cff49439cb39
Ramus Jabee Lloyd Wilson <ramus@rjlwjr.com>
2025-06-15 23:28:42 +0000

    Added DAP support for Go

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,19 +1,66 @@
+---@diagnostic disable: undefined-field
 return {
+  -- https://www.youtube.com/watch?v=yx611gDdysc
   "mfussenegger/nvim-dap",
+  -- vim.keymap.set({ "n", "i" }, "<F8>", "<cmd>DapToggleBreakpoint<cr>", { desc = "DAP: Toggle Breakpoint" })
 
+  keys = {
+    {
+      "<F5>",
+      function()
+        require("dap").continue()
+      end,
+      desc = "DAP: Continue/Start Debugging",
+    },
+    {
+      "<F9>",
+      function()
+        require("dap").toggle_breakpoint()
+      end,
+      desc = "Toggle Breakpoint",
+    },
+    {
+      "<Leader>dbb",
+      function()
+        require("dap").toggle_breakpoint()
+      end,
+      desc = "Toggle Breakpoint",
+    },
+    {
+      "<Leader>dbB",
+      function()
+        require("dap").set_breakpoint()
+      end,
+      desc = "DAP: Set Breakpoint with Condition",
+    },
+    {
+      "<Leader>dbl",
+      function()
+        require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+      end,
+      desc = "DAP: Set Logpoint",
+    },
+    {
+      "<Leader>drl",
+      function()
+        require("dap").run_last()
+      end,
+      desc = "DAP: Run Last Debug Session",
+    },
+  },
   dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "nvim-neotest/nvim-nio",
+    { "rcarriga/nvim-dap-ui", dependencies = { "nvim-neotest/nvim-nio" } },
     "theHamsta/nvim-dap-virtual-text",
-    "leoluz/nvim-dap-go",
+    { "leoluz/nvim-dap-go", lazy = true, ft = { "go" } },
   },
   config = function()
     local dap = require("dap")
     local dapui = require("dapui")
-
     require("dap-go").setup()
-    require("dapui").setup()
+
+    ---@diagnostic disable-next-line: missing-parameter
     require("nvim-dap-virtual-text").setup()
+    dapui.setup()
 
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
@@ -28,33 +75,38 @@ return {
       dapui.close()
     end
 
-    vim.keymap.set({ 'n', 'i' }, '<F5>', "<cmd>DapContinue<cr>", { desc = 'DAP: Continue' })
-    vim.keymap.set({ 'n', 'i' }, '<F10>', "<cmd>DapStepOver<cr>", { desc = 'DAP: Step Over' })
-    vim.keymap.set({ 'n', 'i' }, '<F11>', "<cmd>DapStepInto<cr>", { desc = 'DAP: Step Into' })
-    vim.keymap.set({ 'n', 'i' }, '<F12>', "<cmd>DapStepOut<cr>", { desc = 'DAP: Step Out' })
-    vim.keymap.set({ 'n', 'i' }, '<F8>', "<cmd>DapToggleBreakpoint<cr>", { desc = 'DAP: Toggle Breakpoint' })
-    vim.keymap.set({ 'n', 'i' }, '<F9>', function()
-      dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
-    end, { desc = 'DAP: Set Log Point' })
+    vim.keymap.set("", "<F10>", function()
+      dap.step_over()
+    end, { desc = "DAP: Step Over" })
 
-    vim.keymap.set('n', '<Leader>dr', function() dap.repl.open() end, { desc = 'DAP: Open REPL' })
-    vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end, { desc = 'DAP: Run Last' })
+    vim.keymap.set("", "<F11>", function()
+      dap.step_into()
+    end, { desc = "DAP: Step Into" })
 
-    vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
-      require('dap.ui.widgets').hover()
-    end, { desc = 'DAP: Hover' })
+    vim.keymap.set("", "<F12>", function()
+      dap.step_out()
+    end, { desc = "DAP: Step Out" })
 
-    vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
-      require('dap.ui.widgets').preview()
-    end, { desc = 'DAP: Preview' })
+    vim.keymap.set("n", "<Leader>dir", function()
+      dap.repl.open()
+    end, { desc = "DAP: Open REPL" })
 
-    vim.keymap.set('n', '<Leader>df', function()
-      local widgets = require('dap.ui.widgets')
+    vim.keymap.set({ "n", "v" }, "<eader>duh", function()
+      require("dap.ui.widgets").hover()
+    end, { desc = "DAP: Hover Variables" })
+
+    vim.keymap.set({ "n", "v" }, "<Leader>dup", function()
+      require("dap.ui.widgets").preview()
+    end, { desc = "DAP: Preview Expression" })
+
+    vim.keymap.set("n", "<Leader>duf", function()
+      local widgets = require("dap.ui.widgets")
       widgets.centered_float(widgets.frames)
-    end, { desc = 'DAP: Show Frames' })
+    end, { desc = "DAP: Show Call Stack (Frames)" })
 
-    vim.keymap.set('n', '<Leader>ds', function()
-      require('dap.ui.widgets').toggle_scopes()
-    end, { desc = 'DAP: Toggle Scopes' })
-  end
+    vim.keymap.set("n", "<Leader>dis", function()
+      local widgets = require("dap.ui.widgets")
+      widgets.centered_float(widgets.scopes)
+    end, { desc = "DAP: Show Variable Scopes" })
+  end,
 }
