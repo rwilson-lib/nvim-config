69f9e02a313b353b4bba9d12e351bcce01fa293a
Ramus Jabee Lloyd Wilson <ramus@rjlwjr.com>
2025-06-15 23:37:57 +0000

    lock files created by Lazy

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     -- Check if the operator was 'y' (yank)
     -- And if the selection type was 'v' (character-wise), 'V' (line-wise), or 'b' (block-wise)
     if event.operator == "y" and (event.regtype == "v" or event.regtype == "V" or event.regtype == "b") then
-      vim.highlight.on_yank { higroup = "IncSearch", timeout = 150 }
+      vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
     end
   end,
   desc = "Highlight yanked text (all types)",
