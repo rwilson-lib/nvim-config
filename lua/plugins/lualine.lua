8ef8413ea86fe735ec7d31ec6889021a0ca5530e
Ramus Jabee Lloyd Wilson <ramus@rjlwjr.com>
2025-06-21 08:43:00 +0000

    Added indicator for MCP-tools

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,35 +1,43 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local function win_buf_component()
       local win = vim.fn.winnr()
       local buf = vim.api.nvim_get_current_buf()
       return string.format("%d:%d", win, buf)
     end
-    require('lualine').setup {
+    require("lualine").setup({
       options = {
-        ignore_focus = { "undotree", "neo-tree", "dbee", "dapui_scopes", "dapui_stacks", "dapui_watches", "dapui_breakpoints", },
+        ignore_focus = {
+          "undotree",
+          "neo-tree",
+          "dbee",
+          "dapui_scopes",
+          "dapui_stacks",
+          "dapui_watches",
+          "dapui_breakpoints",
+        },
       },
       sections = {
         lualine_a = {
-          { win_buf_component, },
-          'mode'
+          { win_buf_component },
+          "mode",
         },
-        lualine_b = { 'branch' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
+        lualine_b = { "branch" },
+        lualine_c = { "filename" },
+        lualine_x = { "encoding", "fileformat", "filetype", { require("mcphub.extensions.lualine") } },
+        lualine_y = { "progrmss" },
+        lualine_z = { "location" },
       },
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = { win_buf_component, 'filename' },
+        lualine_c = { win_buf_component, "filename" },
         lualine_x = {},
         lualine_y = {},
         lualine_z = {},
       },
-    }
-  end
+    })
+  end,
 }
