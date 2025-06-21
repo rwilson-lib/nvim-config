21f80b7d430126660a39f10e76412a50ddc7eebd
Ramus Jabee Lloyd Wilson <ramus@rjlwjr.com>
2025-06-21 07:03:04 +0000

    Rename file to ai-assistants to include ai-assistants plugins

--- lua/plugins/ai-assistants.lua
+++ lua/plugins/ai-assistants.lua
@@ -1,5 +1,15 @@
 return {
   {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+    config = function()
+      require("mcphub").setup()
+    end,
+  },
+  { -- AI code suggestion
     "Exafunction/windsurf.vim",
     event = "BufEnter",
   },
@@ -53,13 +63,12 @@ return {
           mcphub = {
             callback = "mcphub.extensions.codecompanion",
             opts = {
-              make_vars = true,
-              make_slash_commands = true,
-              show_result_in_chat = true,
+              show_result_in_chat = true, -- Show mcp tool results in chat
+              make_vars = true, -- Convert resources to #variables
+              make_slash_commands = true, -- Add prompts as /slash commands
             },
           },
         },
-
         adapters = {
           gemini = function()
             return require("codecompanion.adapters").extend("gemini", {
