b27a684753355c96e604abcfdb000e9ff35a5964
Ramus Jabee Lloyd Wilson <ramus@rjlwjr.com>
2025-07-08 16:10:23 +0000

    Added configuration for projects

--- lua/plugins/neovim-project.lua
+++ lua/plugins/neovim-project.lua
@@ -32,8 +32,21 @@ return {
       "~/.config/*",
       "~/Documents/Projects/*",
     },
+    session_manager_opts = {
+      autosave_ignore_dirs = {
+        vim.fn.expand("~"), -- don't create a session for $HOME/
+        "/tmp",
+        "~/tmp",
+      },
+    },
     picker = {
       type = "telescope", -- or "fzf-lua"
+      preview = {
+        enabled = true, -- show directory structure in Telescope preview
+        git_status = true, -- show branch name, an ahead/behind counter, and the git status of each file/folder
+        git_fetch = false, -- fetch from remote, used to display the number of commits ahead/behind, requires git authorization
+        show_hidden = true, -- show hidden files/folders
+      },
     },
   },
   init = function()
