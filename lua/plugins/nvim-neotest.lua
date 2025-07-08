b7ad176a0dffcac0c6c28ba7244089bd8d06af06
Ramus Jabee Lloyd Wilson <ramus@rjlwjr.com>
2025-07-08 16:20:59 +0000

    Add neotest-jest adapter and watch mode keymap for testing
    - Added "nvim-neotest/neotest-jest" plugin for Jest support
    - Configured neotest to use jest.config.ts and current working directory
    - Added keymap <leader>tw to run Jest in watch mode
    - Improved diagnostic message formatting and output panel layout

--- lua/plugins/nvim-neotest.lua
+++ lua/plugins/nvim-neotest.lua
@@ -6,16 +6,22 @@ return {
     "antoinemadec/FixCursorHold.nvim",
     "nvim-treesitter/nvim-treesitter",
     "nvim-neotest/neotest-go",
+    "nvim-neotest/neotest-jest",
   },
   opts = {},
   config = function()
+    vim.api.nvim_set_keymap(
+      "n",
+      "<leader>tw",
+      "<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>",
+      {}
+    )
     -- get neotest namespace (api call creates or returns namespace)
     local neotest_ns = vim.api.nvim_create_namespace("neotest")
     vim.diagnostic.config({
       virtual_text = {
         format = function(diagnostic)
-          local message =
-              diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+          local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
           return message
         end,
       },
@@ -29,14 +35,22 @@ return {
             coverage = true, -- 🔧 must be enabled
           },
         }),
+        require("neotest-jest")({
+          -- jestCommand = "npx jest --",
+          jestConfigFile = "jest.config.ts",
+          -- env = { CI = true },
+          cwd = function(_)
+            return vim.fn.getcwd()
+          end,
+        }),
       },
       output_panel = {
         enabled = true,
-        open = 'botright split | resize 15',
+        open = "botright split | resize 15",
       },
       quickfix = {
         open = false,
       },
     })
-  end
+  end,
 }
